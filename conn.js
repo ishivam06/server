d9c872d350515bd6b2fd388069baa5805c372773
Bhushankolpe <92618968+Bhushankolpe@users.noreply.github.com>
2022-06-28 11:12:50 +0530

    Add files via upload

---
+++ conn.js
@@ -0,0 +1,21 @@
+// const mongoose=require("mongoose");
+// const DB=process.env.DATABASE;
+// mongoose.connect(DB).then(()=>console.log("Data base connected")).catch((error)=>console.log("error"+ error.message));
+const mongoose = require("mongoose");
+
+const connectDB = async() => {
+	try {
+		const conn = await mongoose.connect(process.env.DATABASE, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+	} catch(error) {
+		console.log(`error: ${error.message}`);
+		// console.log(error);
+		process.exit();
+	}
+};
+
+module.exports = connectDB;
